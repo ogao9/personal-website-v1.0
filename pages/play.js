8f6afc96e2c3ca656f0a568239d65a33b801a4ba
unknown <ogao@umich.edu>
2021-07-26 15:59:42 -0400

    delete meta tag

--- pages/play.js
+++ pages/play.js
@@ -4,9 +4,11 @@ import Intro from "../components/Intro";
 export default function play() {
     return (
         <>
-            <Meta title="Play a game" />
+            
             <Intro title="Play" subtitle="Play a fun and quick game while you're here" />
             <h1>Under Construction</h1>
         </>
     );
 }
+
+//<Meta title="Play a game" />
